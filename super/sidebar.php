cad9637b7591901d0c0b61a3183613170f310797
cindyynkhr <143568079+cindyynkhr@users.noreply.github.com>
2024-12-10 13:32:54 +0700

    10

--- super/sidebar.php
+++ super/sidebar.php
@@ -10,28 +10,28 @@
             <a href="dashboard.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-home"></i> Dashboard</a>
         </li>
-        <?php echo $_SERVER["REQUEST_URI"] == '/dataPelanggaran' ? '<li class="active">' : '<li>' ?>
-            <a href="/dataPelanggaran" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
+        <?php echo $_SERVER["REQUEST_URI"] == 'dosen.php' ? '<li class="active">' : '<li>' ?>
+            <a href="dosen.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-book"></i> Kelola Dosen</a>
         </li>
-        <?php echo $_SERVER["REQUEST_URI"] == '/dataPelanggaran' ? '<li class="active">' : '<li>' ?>
-            <a href="/dataPelanggaran" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
+        <?php echo $_SERVER["REQUEST_URI"] == 'mahasiswa.php' ? '<li class="active">' : '<li>' ?>
+            <a href="mahasiswa.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-book"></i> Kelola Mahasiswa</a>
         </li>
         <?php echo $_SERVER["REQUEST_URI"] == 'admin.php' ? '<li class="active">' : '<li>' ?>
             <a href="admin.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-book"></i> Kelola Admin</a>
         </li>
-        <?php echo $_SERVER["REQUEST_URI"] == '/dataMahasiswa' ? '<li class="active">' : '<li>' ?>
-            <a href="/dataMahasiswa" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
+        <?php echo $_SERVER["REQUEST_URI"] == 'dataPelanggaran.php' ? '<li class="active">' : '<li>' ?>
+            <a href="dataPelanggaran.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-user-shield"></i> Data Pelanggaran</a>
         </li>
-        <?php echo $_SERVER["REQUEST_URI"] == '/cetakSurat' ? '<li class="active">' : '<li>' ?>
-            <a href="/cetakSurat" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
+        <?php echo $_SERVER["REQUEST_URI"] == 'informasi.php' ? '<li class="active">' : '<li>' ?>
+            <a href="informasi.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-info-circle"></i> Informasi</a>
         </li>
-        <?php echo $_SERVER["REQUEST_URI"] == '/informasi' ? '<li class="active">' : '<li>' ?>
-            <a href="/informasi" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
+        <?php echo $_SERVER["REQUEST_URI"] == 'logAktivitas.php' ? '<li class="active">' : '<li>' ?>
+            <a href="logAktivitas.php" class="text-decoration-none px-3 py-3 d-block fw-bold"><i
                 class="fas fa-history"></i> Log Aktivitas</a>
         </li>
     </ul>
